795f627de33ea56275a680c73e358356f921bb1d
AbdelrahmanNasser00 <abdo2652149@gmail.com>
2024-02-08 08:36:13 +0200

    Add some configrations for deployment

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { logout } from "./shared/utils/auth";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://realtime-lecturing-api.onrender.com/api",
   timeout: 5000,
 });
 
